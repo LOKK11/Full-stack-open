5913e6512ecfcf41cf05412802407526a96aa771
L0KK11 <myllymakiossi@gmail.com>
2024-11-08 11:16:24 +0200

    Ex. 2.6.-2.10.

---
+++ Osa2/App2/src/App.jsx
@@ -0,0 +1,118 @@
+import { useState } from 'react'
+
+const Filter = ({ searchWith, handleSearchChange }) => {
+  return (
+    <div>
+      Filter shown with:
+      <input
+        value={searchWith}
+        onChange={handleSearchChange}
+      />
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name:
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        number:
+        <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ personsToShow }) => {
+  return (
+    <ul>
+      {
+        personsToShow.map(person =>
+          <li key={person.name}>{person.name} {person.number}</li>
+        )
+      }
+    </ul>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchWith, setSearchWith] = useState('')
+  const personsToShow = searchWith === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(searchWith.toLowerCase()))
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearchWith(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <h2>Search</h2>
+      <Filter
+        searchWith={searchWith}
+        handleSearchChange={handleSearchChange}
+      />
+      <h2>Add a new</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons personsToShow={personsToShow} />
+    </div>
+  )
+
+}
+
+export default App
